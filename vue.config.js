efb07ea59de78562592ba426b43b6f372c5a9d2b
sloh0008-30572622 <sloh0008@student.monash.edu>
2025-05-01 10:35:49 +1000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack')
+const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3007
+    port: 3007,
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    }
   },
   pages: {
     index: {
@@ -13,10 +18,33 @@ module.exports = defineConfig({
     }
   },
   configureWebpack: {
+    experiments: {
+      asyncWebAssembly: true, // ADD this for wllama
+      topLevelAwait: true,    // (some versions of wllama need this)
+    },
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true)
       })
-    ]
+    ],
+    resolve: {
+      fallback: {
+        fs: false,
+        path: false,
+        crypto: false
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.wasm$/,
+          type: "webassembly/async", // <-- CHANGE this
+        },
+        {
+          test: /\.worker\.js$/,
+          use: { loader: 'worker-loader' } // <-- ADD worker-loader for .worker.js
+        }
+      ]
+    }
   }
 })
